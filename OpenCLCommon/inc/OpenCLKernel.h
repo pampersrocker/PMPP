661c7cec7459c866ca6d88cff527ae33e39cd4a7
Marvin Pohl <mp078@hdm-stuttgart.de>
2015-02-01 00:31:51 +0000

    Added documentation to the OpenCLKernel

--- OpenCLCommon/inc/OpenCLKernel.h
+++ OpenCLCommon/inc/OpenCLKernel.h
@@ -133,15 +133,25 @@ public:
 	template< typename T >
 	OpenCLKernelArgument CreateArgumentValue( T value );
 
+	/**
+	@brief Gets the OpenCLKernelArugment at the given position
+	Will create an assertion fail on DEBUG if the given index is >= the number of arguments provided
+	
+	@param index The index of the Argument which should be returned.
+	
+	@return The OpenCLKernelArgument at the position given by index
+	*/
 	OpenCLKernelArgument GetArgument( size_t index );
+
 	/**
 	@brief Adds a given arg to the argument list of the kernel.
 	The arg itself is just a small proxy class which can be copied with no harm
 	
 	*/
 	size_t AddArgument( OpenCLKernelArgument arg );
+
 	/**
-	\brief Clears the stored vector of arguments.
+	@brief Clears the stored vector of arguments.
 
 	Useful for freeing up still referenced buffers or preparing kernel for another run with new parameters 
 	(BeginArgs also calls this).
@@ -157,20 +167,79 @@ public:
 	void BeginArgs();
 
 	/**
-	\brief Pushes the arguments to the OpenCL internal kernel and concludes the arguments passed to this kernel.
+	@brief Pushes the arguments to the OpenCL internal kernel and concludes the arguments passed to this kernel.
 	Subsequents adds are not visible to the CL Kernel unless EndArgs or SetArgs is called again.
 	
 	*/
 	void EndArgs();
 
+	/**
+	@brief Sets the number of threads used by a work group when executing this kernel on the given Dimension
+
+	If dimension is bigger or equal to the IndexDimension of the Kernel, the compilation will fail.
+	
+	@tparam Dimension The dimension at which the WorkSize will be set, 
+	needs to be within 0 and IndexDimension of the Kernel
+	@param size The number of threads in this Dimension, 
+	needs to be within 0 and Maximum Number of Items in this Dimension
+	
+	*/
 	template<unsigned int Dimension>
 	void SetWorkSize( size_t size );
+
+	/**
+	@brief Sets the number of groups started when the kernel is run in the given Dimension
+	The overall work-item count for this Dimension will be determined by the following: groupCount*workSize
+	
+	If dimension is bigger or equal to the IndexDimension of the Kernel, the compilation will fail.
+
+	@param size The number of groups used in this Dimension	
+	*/
 	template<unsigned int Dimension>
 	void SetGroupCount( size_t size );
-	void Release();
-	void Run();
+	
+	/**
+	@brief Runs the kernel with the given Arguments, local and global Work Sizes.
 
+	Usually before calling run the Arguments are set and the WorkSize and GroupCount is adjusted:
+	@code{.cpp}
+	kernel->BeginArgs();
+	//... Set your Args
+	kernel->EndArgs();
+	kernel->SetWorkSize< YourDimension >(YourWorkSize);
+	kernel->SetGroupCount< YourDimension >(YourGroupCount);
+	kernel->Run();
+	kernel->WaitForKernel(); //Optional if you need the result immediatly
+	@endcode
+	
+	*/
+	void Run();
+	
+	/**
+	@brief Releases all data allocated by the kernel and the kernel itself and clears the Arguments
+	The object will be unusable after this Method call and might crash on any Method invocation.
+	
+	*/
+	void Release();
+	
+
+	
+	/**
+	@brief Returns the CommandQueue associated with the kernel, will be set to the default CommandQueue of the context 
+	in which the kernel is generated.
+	
+	The kernel is not the owner of the OpenCLCommandQueue Pointer, it just stores a reference to it.
+
+	@return The currently used command queue by the kernel
+	*/
 	OpenCLCommandQueue* CommandQueue() const;
+	
+	/**
+	@brief Associates the kernel with the given OpenCLCommandQueue.
+	
+	@param val The new OpenCLCommandQueue to be used by the kernel.
+	
+	*/
 	void CommandQueue( OpenCLCommandQueue* val );
 
 	/**
@@ -196,12 +265,29 @@ public:
 	*/
 	cl_ulong QueryPrivateMemSize();
 
-
-
+	/**
+	@brief Gets the OpenCLContext from which the kernel is generated from. The context pointer can also be used to get 
+	the device and platform for which the kernel is created.
+	Kernel Global Arguments can be interchanged freely between kernels that share the same Context.
+	
+	
+	@return The OpenCLContext which has been used to generate this Kernel.
+	*/
 	const OpenCLContext* const Context() const;
 
+	
+	/**
+	@brief Sets the Stored arguments to the actual kernel implementation, same as EndArgs(), but might look awkward to 
+	call EndArgs() when changed an already set Argument in the middle.
+	
+	*/
 	void SetArgs();
 
+	
+	/**
+	@brief Waits for the kernels completion, returns as soon as the kernel finished.
+	
+	*/
 	void WaitForKernel();
 protected:
 private:
